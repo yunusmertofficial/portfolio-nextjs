a3cb4bf7254a269fde50e04a76d9c12546a0185e
yunusmertofficial <yunusmertofficial@gmail.com>
2023-10-21 22:40:03 +0300

    updated layout js

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,7 +7,7 @@ import { ThemeProvider } from "@/context/ThemeContext";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Lama Dev",
+  title: "Yunus Emre Mert",
   description: "This is the description",
 };
 
